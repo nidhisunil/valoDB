3d1203c2872ed3d13fd709df79b73cfa47daed23
Nidhi <nidhisunil2000@gmail.com>
2022-06-30 23:43:44 -0300

    starting files and get method

---
+++ app.js
@@ -0,0 +1,45 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/valoDB",{useNewUrlParser: true});
+const agentSchema = {
+  name: {
+    type: String
+  },
+  type: {
+    type: String
+  },
+  ability: [{
+    type: String
+  }]
+};
+const Agent = mongoose.model("Agent", agentSchema);
+
+app.get("/agents",function(req,res){
+  Agent.find(function(err, foundAgents){
+    if(!err){
+      res.send(foundAgents);
+    }else{
+      res.send(err);
+    }
+
+  });
+});
+
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
